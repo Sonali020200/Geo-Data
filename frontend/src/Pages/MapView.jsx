37afcf5d1904fa758153dbdb66aa04020fa4272f
Sonali Burman <sonali.b.020200@gmail.com>
2024-03-21 19:46:57 +0530

    geoData

--- frontend/src/Pages/MapView.jsx
+++ frontend/src/Pages/MapView.jsx
@@ -29,7 +29,7 @@ const MapView = () => {
   useEffect(() => {
     const fetchCoordinates = async () => {
       try {
-        const response = await axios.get('http://localhost:8080/geodata/list');
+        const response = await axios.get(' ');
         const userid = localStorage.getItem("userId");
         const data = response.data
           .filter((e) => e.user_id === userid)
